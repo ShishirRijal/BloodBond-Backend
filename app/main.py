7a807f1db3a050848e9425bb27e77dfd444e3c57
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2023-12-24 12:47:59 +0545

    removed sqlalchemy table create command

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from routes import *
 
-try:
-    Base.metadata.create_all(bind=engine)
-    print("Tables created successfully.")
-except Exception as e:
-    print(f"Error creating tables: {e}")
+# try:
+#     Base.metadata.create_all(bind=engine)
+#     print("Tables created successfully.")
+# except Exception as e:
+#     print(f"Error creating tables: {e}")
 
 
 # Initialize FastAPI App
