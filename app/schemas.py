340aa32bdd0ab1b9a84c3b2a2b553dfd3c0c432d
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2023-12-24 15:47:43 +0545

    register donor endpoint added

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 # This file contains the Pydantic models,
 # which are used to define the schema for validation.
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
 from datetime import datetime
 
 
@@ -59,3 +59,34 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     email: EmailStr | None = None
+
+
+class DonorBase(BaseModel):
+    email: EmailStr
+    first_name: str
+    last_name: str
+    phone: str
+    sex: str
+    date_of_birth: datetime
+    blood_group: str
+
+
+class DonorCreate(DonorBase):
+    password: str
+
+    @validator("date_of_birth", pre=True)
+    def parse_date_of_birth(cls, value):
+        try:
+            # Parse the input string into a datetime object
+            return datetime.strptime(value, "%Y-%m-%d")
+        except ValueError:
+            raise ValueError(
+                "Invalid date format. Please provide the date in YYYY-MM-DD format.")
+
+
+class DonorResponse(DonorBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
--- routes/donors.py
+++ routes/donors.py
@@ -1,13 +1,35 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException, status
+import psycopg2
 from sqlalchemy.orm import Session
+from app import schemas
 
 from app.database import get_db
+from app.utils import get_password_hash
 import models
 
 router = APIRouter(
     prefix="/api/v1/donors",
     tags=["Donors"]
 )
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=schemas.DonorResponse)
+def register_donor(donor: schemas.DonorCreate, db: Session = Depends(get_db)):
+    try:
+        donor.password = get_password_hash(donor.password)
+        new_donor = models.Donor(**donor.model_dump())
+        db.add(new_donor)
+        db.commit()
+        db.refresh(new_donor)
+        return new_donor
+    except (Exception, psycopg2.IntegrityError) as error:
+        print("register donor error: ", error)
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=f"User already exists")
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong!")
 
 
 @router.get("/")
