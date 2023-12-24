e2db7719221ab7ec99da7523e3d690ad910b84d9
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2023-12-24 19:17:08 +0545

    removed auth signup route

--- routes/auth.py
+++ routes/auth.py
@@ -15,22 +15,22 @@ router = APIRouter(
 )
 
 
-@router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    try:
-        user.password = get_password_hash(user.password)
-        new_user = User(**user.model_dump())
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
-        return new_user
-    except (Exception, psycopg2.IntegrityError) as error:
-        # if error.pgcode == errorcodes.UNIQUE_VIOLATION:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail=f"User already exists")
-    except:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal Server Error")
+# @router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     try:
+#         user.password = get_password_hash(user.password)
+#         new_user = User(**user.model_dump())
+#         db.add(new_user)
+#         db.commit()
+#         db.refresh(new_user)
+#         return new_user
+#     except (Exception, psycopg2.IntegrityError) as error:
+#         # if error.pgcode == errorcodes.UNIQUE_VIOLATION:
+#         raise HTTPException(
+#             status_code=status.HTTP_409_CONFLICT, detail=f"User already exists")
+#     except:
+#         raise HTTPException(
+#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Internal Server Error")
 
 
 @router.post("/login", status_code=status.HTTP_200_OK, response_model=schemas.Token)
